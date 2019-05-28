577d7ee64d980b27759d2f3392259e1ccc67fa19
SamiraJashanica <44278561+samirajashanica@users.noreply.github.com>
2019-05-28 03:31:47 +0200

    Add files via upload

---
+++ veturaShitje.php
@@ -0,0 +1,111 @@
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+<meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+<title>Veturat ne Shitje | Rent a CAR |</title>
+
+<link rel="stylesheet" type="text/css" href=  "stili.css">
+
+</head>
+
+<body>
+<img src="images/free-wallpaper-11.jpg" width="1347px" height="320px">
+
+<ul id="nav">
+      <li><a href= "index.html">Ballina</a></li>
+      <li><a href=" rezervo.html">Rezervo </a></li>
+      <li><a href= "veturaShitje.html">Veturat Ne Shitje </a></li>
+      <li><a href= "kontakti.html">Kontakti</a></li>
+   </ul>
+
+
+<!--
+<hr>
+<div class="djathte"> 
+
+		<h2>Mund të na Ndiqni edhe në:</h2>
+		<ul>
+					<li><a href="https://www.facebook.com">Facebook</a></li> 
+			<li><a href="https://twitter.com/">Twitter</a></li>
+			<li><a href="http://www.telegrafi.com">Telegrafi</a></li>	
+				</ul>
+			<hr>
+			
+			</div>
+
+-->
+
+
+
+
+
+<div id="prapavija"><!-- end holder -->
+  <h4>Veturat  me te reja i gjeni ne autosallonin tone ne <strong>Rent a Car "Service"</strong></h4>
+
+  <div id="kutia">
+    <div class="imgHolder">
+      <div class="img"> <a href="AudiGaleria.html"><img src="images/Audi-Q7.jpg" width="249" height="189" /></a>
+        <div class="TitulliFotos">
+          <h3>Audi Q7 </h3>
+          
+   
+          
+          
+          
+        </div>
+        <!-- end imgTitle --> 
+      </div>
+      <!-- end img --> 
+    </div>
+    <!-- end imgHolder -->
+  
+    <div class="imgHolder">
+      <div class="img"> <a href="MercedesGaleria.html"><img src="images/Mercedes.jpg" width="249" height="189" /></a>
+        <div class="TitulliFotos">
+          <h3>Mercedes Benz C220 CDI </h3>
+        </div>
+        <!-- end imgTitle --> 
+        
+      </div>
+      <!-- end img --> 
+    </div>
+    <!-- end imgHolder -->
+    
+    <div class="imgHolder">
+      <div class="img"> <a href="BMWgaleria.html"><img src="images/BMWcover.jpg" width="249" height="189" /></a>
+        <div class="TitulliFotos">
+          <h3>BMW X3</h3>
+          
+
+
+        </div>
+        <!-- end imgTitle --> 
+     
+
+      </div>
+      <!-- end img --> 
+      
+    </div>    
+       
+     <!-- end imgHolder --> 
+    <br class="clearFloat"/>
+  </div> 
+  
+
+  <!-- end images --> 
+  <br />
+  <br />
+  <br />
+  <hr />
+</div>
+
+
+
+
+
+
+
+</body>
+
+</html>

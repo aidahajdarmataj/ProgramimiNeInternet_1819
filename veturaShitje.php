47dfc583ffaef6a586019239396f55172daa3685
SamiraJashanica <44278561+samirajashanica@users.noreply.github.com>
2019-05-31 23:36:45 +0200

    Add files via upload

--- veturaShitje.php
+++ veturaShitje.php
@@ -1,4 +1,4 @@
-﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 
 <head>
@@ -77,6 +77,35 @@
       <div class="img"> <a href="BMWgaleria.html"><img src="images/BMWcover.jpg" width="249" height="189" /></a>
         <div class="TitulliFotos">
           <h3>BMW X3</h3>
+        </br>
+          </br>
+            </br>
+              </br>
+              <?php
+$arr = array('Rent','a','car');
+echo implode(" ",$arr)."<br>";
+echo implode("+",$arr)."<br>";
+echo implode("-",$arr)."<br>"; 
+echo implode("X",$arr);
+?>
+
+
+
+  
+
+</br>
+</br>
+</br>
+</br>
+
+
+
+
+
+
+
+
+
 
 
 <script>
@@ -116,15 +145,15 @@ function showUser(str) {
 <div id="txtHint"><b>Person info will be listed here...</b></div>
 
 <?php
-$tipi = $_GET['tipi'];
+$q = intval($_GET['q']);
 
-$con = mysqli_connect('localhost','root','');
+$con = mysqli_connect('localhost','phpdatabase');
 if (!$con) {
     die('Could not connect: ' . mysqli_error($con));
 }
 
-mysqli_select_db($con,"phpdatabase");
-$sql="SELECT * FROM phptable WHERE id = '".$tipi."'";
+mysqli_select_db($con,"ajax_demo");
+$sql="SELECT * FROM user WHERE id = '".$q."'";
 $result = mysqli_query($con,$sql);
 
 echo "<table>
@@ -135,25 +164,116 @@ echo "<table>
 <th>Shpejtesia</th>
 
 </tr>";
-while($row = mysqli_fetch_array($result)) {
+while($row = mysqli_fetch_array($result)) 
+ {
     echo "<tr>";
     echo "<td>" . $row['ID'] . "</td>";
     echo "<td>" . $row['Emri'] . "</td>";
     echo "<td>" . $row['Tipi'] . "</td>";
     echo "<td>" . $row['Shpejtesia'] . "</td>";
-    
+ 
     echo "</tr>";
 }
 echo "</table>";
 mysqli_close($con);
 ?>
-
         </br>
       </br>
     </br>
       </br>
 
+<?php
+// define variables and set to empty values
+$nameErr = $emailErr = $genderErr = $websiteErr = "";
+$name = $email = $gender = $comment = $website = "";
 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if (empty($_POST["name"])) {
+    $nameErr = "Name is required";
+  } else {
+    $name = test_input($_POST["name"]);
+    // check if name only contains letters and whitespace
+    if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
+      $nameErr = "Only letters and white space allowed"; 
+    }
+  }
+  
+  if (empty($_POST["email"])) {
+    $emailErr = "Email is required";
+  } else {
+    $email = test_input($_POST["email"]);
+    // check if e-mail address is well-formed
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      $emailErr = "Invalid email format"; 
+    }
+  }
+    
+  if (empty($_POST["website"])) {
+    $website = "";
+  } else {
+    $website = test_input($_POST["website"]);
+    // check if URL address syntax is valid
+    if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$website)) {
+      $websiteErr = "Invalid URL"; 
+    }    
+  }
+
+  if (empty($_POST["comment"])) {
+    $comment = "";
+  } else {
+    $comment = test_input($_POST["comment"]);
+  }
+
+  if (empty($_POST["gender"])) {
+    $genderErr = "Gender is required";
+  } else {
+    $gender = test_input($_POST["gender"]);
+  }
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+?>
+
+<h2>Validimi</h2>
+<p><span class="error">* required field</span></p>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
+  Name: <input type="text" name="name">
+  <span class="error">* <?php echo $nameErr;?></span>
+  <br><br>
+  E-mail: <input type="text" name="email">
+  <span class="error">* <?php echo $emailErr;?></span>
+  <br><br>
+  Website: <input type="text" name="website">
+  <span class="error"><?php echo $websiteErr;?></span>
+  <br><br>
+  Comment: <textarea name="comment" rows="5" cols="40"></textarea>
+  <br><br>
+  Gender:
+  <input type="radio" name="gender" value="female">Female
+  <input type="radio" name="gender" value="male">Male
+  <input type="radio" name="gender" value="other">Other
+  <span class="error">* <?php echo $genderErr;?></span>
+  <br><br>
+  <input type="submit" name="submit" value="Submit">  
+</form>
+
+<?php
+echo "<h2>Your Input:</h2>";
+echo $name;
+echo "<br>";
+echo $email;
+echo "<br>";
+echo $website;
+echo "<br>";
+echo $comment;
+echo "<br>";
+echo $gender;
+?>
 
 
 
